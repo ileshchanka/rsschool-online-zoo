9807431d071c494c34239a6a0f4860dbbf39edfe
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-03-10 11:57:21 +0100

    feat: add ESLint Configuration

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  ...tsPlugin.configs["flat/recommended"],
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
